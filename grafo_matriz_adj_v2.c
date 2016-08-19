c25059ab7141f3a011d6d1584fb4f826de31f2c5
Forvisk <adrianojr11@gmaiol.com>
2016-08-19 13:24:18 -0300

    upload n

--- grafo_matriz_adj_v2.c
+++ grafo_matriz_adj_v2.c
@@ -4,6 +4,8 @@ int main(){
 	
 	int grafo[20][20][2];
 	
+	int **grafo2, **flag2;
+	
 	FILE *fp = NULL;
 	int nVert = 0, nArest = 0;
 	char arest, direcional = 'n';
@@ -25,22 +27,59 @@ int main(){
 	fscanf( fp, "%i\n", &nArest);
 	printf("%i\n", nArest);
 	
+	//alocação teste
+	grafo2 = (int**)malloc(sizeof(int) * nVert);
+	flag2 = (int**)malloc(sizeof(int) * nVert);
+	for(i = 0; i < nVert; i++){	
+		grafo2[i] = (int**)malloc(sizeof(int) * nVert);
+		flag2[i] = (int**)malloc(sizeof(int) * nVert);
+	}
+	
+	//zerar vertices
+	for(i = 0; i< nVert; i++)
+		for(j = 0; j<nVert; j++){
+			grafo2[i][j] = 0;
+			flag2[i][j] = 0;
+		}
+	
 	//leitura do arquivo para grafo
 	for (i = 0; i < nArest; i++){
 		fscanf( fp, "%c: %i, %i;\n", &arest, &a, &b);
-		printf("%c: %i, %i\n", arest, a, b);
-		++grafo[--a][--b][0];
-		grafo[a][b][1] = 0;
+		printf(".0");
 		
-		if((direcional == 'n')&&(a!=b)){
+		printf("%c: %i, %i\n", arest, a, b);
+		printf(".1");
+		
+		++grafo[a][b][0];
+		grafo[a][b][1] = 0;
+		printf(".2");
+		
+		++grafo2[a][b];
+		flag2[a][b] = 0;
+		printf(".3");
+		
+		if(direcional == 'n'){
 			++grafo[b][a][0];
 			grafo[b][a][1] = 0;
+			printf(".4");
+		
+			grafo2[b][a] += 1;
+			flag2[b][a] = 0;
+			printf(".5");
 		}
 	}
 	
 	for (i= 0; i< nVert; i++){
 		for (j = 0; j< nVert; j++){
 			printf("%i ", grafo[i][j][0]);
+		}
+		printf("\n");
+	}
+	
+	
+	for (i= 0; i< nVert; i++){
+		for (j = 0; j< nVert; j++){
+			printf("%i ", grafo2[i][j]);
 		}
 		printf("\n");
 	}
