533a5fd64d2f7eb15dc3a551f6db3a40175014db
Forvisk <adrianojr11@gmail.com>
2016-08-19 11:36:37 -0300

    aualizar arquivo

--- grafo_matriz_adj_v1.c
+++ grafo_matriz_adj_v1.c
@@ -1,39 +1,52 @@
 #include <stdio.h>
 
 int main(){
-	int grafo[20][20][2];
+	
+	int grafo[20][20], flag[20][20];
+	
 	FILE *fp = NULL;
 	int nVert = 0, nArest = 0;
 	char arest, direcional = 'n';
 	
-	printf("%i", grafo[2][6][1]);
 	
+	int a, b, i;
+	
+	printf("%i", grafo[2][6]);
+	
+	//Abertura do arquivo
 	fp = fopen("grafo.txt", "r");
 	if(fp == NULL){
 		printf("\nArquivo não existe\n");
 		return 0;
 	}
+	
+	//leitura do arquivo para vertica, direcionado e arestas
 	fscanf( fp, "%c\n", &direcional);
 	fscanf( fp, "%i\n", &nVert);
 	printf("%i\n", nVert);
 	fscanf( fp, "%i\n", &nArest);
 	printf("%i\n", nArest);
-	int a, b, i;
-	for (int i = 0; i < nArest; i++){
+	
+	
+	
+	//leitura do arquivo para grafo
+	for (i = 0; i < nArest; i++){
 		fscanf( fp, "%c: %i, %i;\n", &arest, &a, &b);
 		printf("%c: %i, %i\n", arest, a, b);
-		++grafo[a][b][0];
-		grafo[a][b][1] = 0;
+		++grafo[a][b];
 		if(direcional == 'n'){
-			++grafo[b][a][0];
-			grafo[b][a][1] = 0;
+			++grafo[b][a];
 		}
 	}
 	
-	//grafo completo
 	
+	//grafo completo
 	if(direcional == 'n'){
 		printf("\ngrafo não direcionado: implementação necessaria");
+		int p[2];
+		/*do{
+			
+		}while(1);*/
 	}else{
 		printf("\ngrafo direcionado: implementação a ser feita");
 	}
